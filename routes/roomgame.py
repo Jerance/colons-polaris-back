7e04b9ac479501a114b2cb1a433c0fb44511c19f
Jerance <michael.jerance@gmail.com>
2023-04-23 12:57:51 +0200

    FEAT/ Get GameRoom by token access

--- routes/roomgame.py
+++ routes/roomgame.py
@@ -8,6 +8,38 @@ router = APIRouter()
 db = firebase.db
 room_ws_connections = {}
 
+
+@router.post("/create/game_room/{player_name}/{token_game_room}")
+async def create_room(player_name: str, token_game_room: str):
+    new_room_ref = db.collection("game_room").document()
+    new_room_ref.set({
+        "room_game_owner": player_name,
+        "token_game_room": token_game_room,
+        "players": []
+    })
+    room_id = new_room_ref.id
+    return {
+        "room_id": room_id,
+        "owner_room_game": player_name,
+        "token_game_room": token_game_room,
+    }
+
+
+@router.post("/join/game_room/{player_name}/{token_game_room}")
+async def join_room(player_name: str, token_game_room: str):
+    game_room_ref = db.collection("game_room").where(
+        "token_game_room", "==", token_game_room).get()
+    if not game_room_ref:
+        return {"message": "Invalid game room token"}
+
+    room_id = game_room_ref[0].id
+    db.collection("game_room").document(room_id).update(
+        {"players": firestore.ArrayUnion([player_name])})
+    await broadcast(room_id, "players_updated")
+
+    return {"message": f"{player_name} has joined the game room {room_id}"}
+
+
 async def broadcast(room_id: str, message: str):
     if room_id in room_ws_connections:
         for ws in room_ws_connections[room_id]:
@@ -49,32 +81,15 @@ async def get_room_by_id(room_id: str):
         return {"message": "Room not found"}
 
 
-@router.post("/create/game_room/{player_name}/{token_game_room}")
-async def create_room(player_name: str, token_game_room: str):
-    new_room_ref = db.collection("game_room").document()
-    new_room_ref.set({
-        "room_game_owner": player_name,
-        "token_game_room": token_game_room,
-        "players": []
-    })
-    room_id = new_room_ref.id
-    return {
-        "room_id": room_id,
-        "owner_room_game": player_name,
-        "token_game_room": token_game_room,
-    }
-
-@router.post("/join/game_room/{player_name}/{token_game_room}")
-async def join_room(player_name: str, token_game_room: str):
-    game_room_ref = db.collection("game_room").where("token_game_room", "==", token_game_room).get()
-    if not game_room_ref:
-        return {"message": "Invalid game room token"}
-
-    room_id = game_room_ref[0].id
-    db.collection("game_room").document(room_id).update({"players": firestore.ArrayUnion([player_name])})
-    await broadcast(room_id, "players_updated")
-
-    return {"message": f"{player_name} has joined the game room {room_id}"}
+@router.get("/game_room/token/{token_game_room}")
+async def get_room_by_token(token_game_room: str):
+    room_ref = db.collection("game_room").where(
+        "token_game_room", "==", token_game_room)
+    room = room_ref.get()
+    if room:
+        return room[0].to_dict()
+    else:
+        return {"message": "Room not found"}
 
 
 @router.delete("/game_room/{room_id}")
